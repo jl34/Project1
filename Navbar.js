751cb6aaec46ebd00864c0b142e4a8dfb52344fb
jl34 <98934221+jl34@users.noreply.github.com>
2022-02-02 20:26:45 -0800

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "./Navbar.css";
+import avatar from "./avatar.svg";
+
+const Navbar = ({ sidebarOpen, openSidebar }) => {
+  return (
+    <nav className="navbar">
+      <div className="nav_icon" onClick={() => openSidebar()}>
+        <i className="fa fa-bars" aria-hidden="true"></i>
+      </div>
+      <div className="navbar__left">
+        <a className="active_link" href="#">Dashboard</a>
+        <a href="#">Users</a>
+        <a href="#">Settings</a>
+      </div>
+      <div className="navbar__right">
+        <a href="#">
+          <i className="fa fa-search" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+          <i className="fa fa-clock-o" aria-hidden="true"></i>
+        </a>
+        <a href="#!">
+          <img width="30" src={avatar} alt="avatar" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
